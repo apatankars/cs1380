419d75bf02c06c6fe36ef6c69bc802e643b47d4d
Armaan Patankar <armaan_patankar@brown.edu>
2025-02-17 03:20:34 +0000

    i hate it here

--- distribution/all/status.js
+++ distribution/all/status.js
@@ -1,10 +1,25 @@
+const distribution = require("@brown-ds/distribution");
 
 const status = function(config) {
   const context = {};
   context.gid = config.gid || 'all';
 
+  cb = (error, value) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(value);
+    }
+  }
+
   return {
     get: (configuration, callback) => {
+      callback = callback || cb;
+      if (configuration === undefined || configuration === null ){
+        callback(new Error('Configuration is required'), null);
+        return;
+      }
+      
     },
 
     spawn: (configuration, callback) => {
--- test/comm.all.test.js
+++ test/comm.all.test.js
@@ -20,14 +20,12 @@ const n5 = {ip: '127.0.0.1', port: 9005};
 const n6 = {ip: '127.0.0.1', port: 9006};
 
 test.only('(2 pts) all.comm.send(status.get(nid))', (done) => {
-  
   const nids = Object.values(mygroupGroup).map((node) => id.getNID(node));
   const remote = {service: 'status', method: 'get'};
 
   distribution.mygroup.comm.send(['nid'], remote, (e, v) => {
     expect(e).toEqual({});
     try {
-      console.log("In the test");
       expect(Object.values(v).length).toBe(nids.length);
       expect(Object.values(v)).toEqual(expect.arrayContaining(nids));
       done();
@@ -79,7 +77,6 @@ test('(2 pts) all.comm.send(status.get(random))', (done) => {
 });
 
 beforeAll((done) => {
-  jest.setTimeout(1000000);
   // First, stop the nodes if they are running
   const remote = {service: 'status', method: 'stop'};
 
