583d153a3a0ff13b730f2e3052824f7c7a5f62dd
Armaan Patankar <armaan_patankar@brown.edu>
2025-02-25 03:12:05 +0000

    cleanup

---
+++ cleanup.js
@@ -0,0 +1,15 @@
+const n1 = { ip: '127.0.0.1', port: 9001 };
+const n2 = { ip: '127.0.0.1', port: 9002 };
+const n3 = { ip: '127.0.0.1', port: 9003 };
+
+const remote = {service: 'status', method: 'stop'};
+remote.node = n1;
+distribution.local.comm.send([], remote, (e, v) => {
+    remote.node = n2;
+    distribution.local.comm.send([], remote, (e, v) => {
+        remote.node = n3;
+            distribution.local.comm.send([], remote, (e, v) => {
+                process.exit(0);
+        })
+    });
+});
